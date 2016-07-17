1ede6d71adc0e3f17e9fe81ab7ebfcb0fc157d37
sitita <sitita_charuraks@hotmail.com>
2016-07-17 12:39:46 +0700

    register+registerpost

--- engine/registerpost.php
+++ engine/registerpost.php
@@ -4,7 +4,7 @@ include_once '../controller/db_functions.php';
 
 $firstname              = $_POST['first_name'];
 $lastname               = $_POST['last_name'];
-$bday                   = $_POST['BirthDay'];
+$bday                   = $_POST['DOBYear'].'-'.$_POST['DOBMonth'].'-'.$_POST['DOBDay'];
 $email                  = $_POST['email'];
 $sex                    = $_POST['Sex'];
 $country                = $_POST['Country'];
@@ -22,7 +22,7 @@ $db->insertregister($firstname, $lastname, $bday, $email, $sex, $country, $passw
 header("Location: ../Theme/login.html");
 function genid($country,$txtid){
 $id = substr($txtid,-8);
-$nation = substr($country,0,2);
+//$nation = substr($country,0,2);
 $nation = strtoupper($nation);
 $result = $nation.$id;
 return $result;
